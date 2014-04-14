6727ea745ff8ba49a820c17bba66a0f97f5830d8
William Robson <logiblocs@gmail.com>
2014-04-14 16:57:14 +0100

    YAML produced now a lot cleaner and easier to read

--- src/net/minecartrapidtransit/path/data/YamlDataStore.java
+++ src/net/minecartrapidtransit/path/data/YamlDataStore.java
@@ -1,6 +1,11 @@
 package net.minecartrapidtransit.path.data;
 
+import java.beans.IntrospectionException;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.Set;
+import java.util.TreeSet;
 
 import net.minecartrapidtransit.path.constants.S;
 import net.minecartrapidtransit.path.core.Connection;
@@ -8,25 +13,60 @@ import net.minecartrapidtransit.path.core.Network;
 import net.minecartrapidtransit.path.core.Place;
 import net.minecartrapidtransit.path.core.Station;
 
+import org.yaml.snakeyaml.TypeDescription;
 import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+import org.yaml.snakeyaml.introspector.BeanAccess;
+import org.yaml.snakeyaml.introspector.Property;
+import org.yaml.snakeyaml.introspector.PropertyUtils;
+import org.yaml.snakeyaml.nodes.Tag;
+import org.yaml.snakeyaml.representer.Representer;
 
 public class YamlDataStore implements DataFormat {
 
 	@Override
 	public String encodeNetwork(Network network) {
+		Representer representer = new Representer();
+		representer.setPropertyUtils(new CustomPropertyUtils());
+		representer.addClassTag(NetworkData.class, new Tag("!network"));
 		NetworkData nd = new NetworkData(network);
-		Yaml yaml = getYaml();
+		Yaml yaml = new Yaml(representer);
 		return yaml.dump(nd);
 	}
 
 	@Override
 	public Network decodeNetwork(String string) {
-		Yaml yaml = getYaml();
+		Constructor constructor = new Constructor();
+		constructor.addTypeDescription(new TypeDescription(NetworkData.class, "!network"));
+		Yaml yaml = new Yaml(constructor);
 		return ((NetworkData) yaml.load(string)).toNetwork();
 	}
 	
-	private Yaml getYaml(){
-		return new Yaml();
+	private class CustomPropertyUtils extends PropertyUtils {
+        @Override
+        protected Set<Property> createPropertySet(Class<? extends Object> type, BeanAccess bAccess)
+                throws IntrospectionException {
+            Set<Property> result = new TreeSet<Property>(new CustomComparator());
+            result.addAll(super.createPropertySet(type, bAccess));
+            return result;
+        }		
+        private class CustomComparator implements Comparator<Property> {
+
+			@Override
+			public int compare(Property o1, Property o2) {
+				String[] special = new String[] {"id", "name"};
+				boolean a = Arrays.asList(special).contains(o1.getName());
+				boolean b = Arrays.asList(special).contains(o2.getName());
+				if(a == b){
+					return o1.getName().compareTo(o2.getName());
+				}else if(a){
+					return -1;
+				}else{
+					return 1;
+				}
+			}
+        	
+        }
 	}
 	
 	//TODO remove this
