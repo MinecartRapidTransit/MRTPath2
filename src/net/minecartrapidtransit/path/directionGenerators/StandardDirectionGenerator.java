3dd790839d176f2cb743ae527b2382ebf07aaadc
William Robson <logiblocs@gmail.com>
2014-04-15 21:02:03 +0100

    New version of network and change to output

--- src/net/minecartrapidtransit/path/directionGenerators/StandardDirectionGenerator.java
+++ src/net/minecartrapidtransit/path/directionGenerators/StandardDirectionGenerator.java
@@ -15,14 +15,15 @@ public class StandardDirectionGenerator implements DirectionGenerator {
 			if(current.getFrom().getId().equals(S.id_START)){
 				return String.format("Walk to the %s.", current.getData());
 			}else if(current.getTo().getId().equals(S.id_END)){
-				return String.format("You have arrived at %s", current.getData());
+				return String.format("You have arrived at %s.", current.getData());
 			}else{
-				return String.format("Travel by foot to %s", current.getData());
+				return String.format("Travel by foot to %s.", current.getData());
 			}
 		case S.type_RAIL:
 		case S.type_HSRAIL:
 			if(next.getType().equals(S.type_TRANSFER) || next.getType().equals(S.type_FOOT)){
-				String ret =  String.format("Ride the %s until you reach %s", originalName, current.getTo().getPlace().getName());
+				if(originalName == null) originalName = current.getData();
+				String ret =  String.format("Ride the %s until you reach %s.", originalName, current.getTo().getPlace().getName());
 				originalName = null;
 				return ret;
 			}else if(originalName == null){
@@ -31,12 +32,14 @@ public class StandardDirectionGenerator implements DirectionGenerator {
 			break;
 		case S.type_BUS:
 			if(next.getType().equals(S.type_TRANSFER) || next.getType().equals(S.type_FOOT)){
-				return String.format("Take the bus, %s, to %s", current.getData(), current.getTo().getPlace().getName());
+				return String.format("Take the bus, %s, to %s.", current.getData(), current.getTo().getPlace().getName());
 			}
 		case S.type_WARP:
 			if(next.getType().equals(S.type_TRANSFER) || next.getType().equals(S.type_FOOT)){
-				return String.format("Take the warp, %s, to %s", current.getData(), current.getTo().getPlace().getName());
+				return String.format("Take the warp, %s, to %s.", current.getData(), current.getTo().getPlace().getName());
 			}
+		case S.type_TRANSFER:
+			return String.format("Transfer to %s.", current.getTo().getName());
 		}
 		return null;
 	}
