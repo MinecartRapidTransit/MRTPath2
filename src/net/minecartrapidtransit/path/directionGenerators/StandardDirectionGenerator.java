45df26e5498aed79e318db65cee4cc5449d1a83b
eschoenawa <emils.emails@gmail.com>
2014-04-22 15:23:16 +0200

    Downgrade to Java 6

--- src/net/minecartrapidtransit/path/directionGenerators/StandardDirectionGenerator.java
+++ src/net/minecartrapidtransit/path/directionGenerators/StandardDirectionGenerator.java
@@ -8,8 +8,7 @@ public class StandardDirectionGenerator implements DirectionGenerator {
 	@Override
 	public String generateDirection(Step current, Step next) {
 		// System.out.printf("%s: %s\n", current.getType(), current.getTo().getId()); // Used for debug
-		switch(current.getType()){
-		case S.type_FOOT:
+		if (current.getType().equals(S.type_FOOT)) {
 			if(current.getFrom().getId().equals(S.id_START)){
 				return String.format("Walk to the %s.", current.getData());
 			}else if(current.getTo().getId().equals(S.id_END)){
@@ -17,21 +16,26 @@ public class StandardDirectionGenerator implements DirectionGenerator {
 			}else{
 				return String.format("Travel by foot to %s.", current.getData());
 			}
-		case S.type_RAIL:
-		case S.type_HSRAIL:
+		}
+		else if (current.getType().equals(S.type_RAIL)) {
+			
+		}
+		else if (current.getType().equals(S.type_HSRAIL)) {
 			if(next.getType().equals(S.type_TRANSFER) || next.getType().equals(S.type_FOOT)){
 				return String.format("Ride the %s until you reach %s.", current.getData(), current.getTo().getPlace().getName());
 			}
-			break;
-		case S.type_BUS:
+		}
+		else if (current.getType().equals(S.type_BUS)) {
 			if(next.getType().equals(S.type_TRANSFER) || next.getType().equals(S.type_FOOT)){
 				return String.format("Take the bus, %s, to %s.", current.getData(), current.getTo().getPlace().getName());
 			}
-		case S.type_WARP:
+		}
+		else if (current.getType().equals(S.type_WARP)) {
 			if(next.getType().equals(S.type_TRANSFER) || next.getType().equals(S.type_FOOT)){
 				return String.format("Take the warp, %s, to %s.", current.getData(), current.getTo().getPlace().getName());
 			}
-		case S.type_TRANSFER:
+		}
+		else if (current.getType().equals(S.type_TRANSFER)) {
 			return String.format("Transfer to the %s.", current.getTo().getName());
 		}
 		return null;
